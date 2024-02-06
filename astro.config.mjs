6b862d2a3937a87653e5991bb67e086cb58394ce
merb <michael.erb@erbwebdev.com>
2024-02-06 11:50:43 -0700

    chore: remove decapcmsoauth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
-import decapCmsOauth from "astro-decap-cms-oauth";
+// import decapCmsOauth from "astro-decap-cms-oauth";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), decapCmsOauth({ oauthDisabled: true })]
+  integrations: [
+    mdx(),
+    sitemap(),
+    // decapCmsOauth({ oauthDisabled: true })
+  ]
 });
