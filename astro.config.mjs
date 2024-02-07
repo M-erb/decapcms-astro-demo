10bbded804c57effa90c97d633d56127719e2f87
merb <michael.erb@erbwebdev.com>
2024-02-06 17:02:31 -0700

    chore: add decapcms oauth integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import decapCmsOauth from "astro-decap-cms-oauth";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [
-    mdx(),
-    sitemap()
-  ]
+  site: 'https://decapcms-astro-demo.pages.dev',
+  integrations: [mdx(), sitemap(), decapCmsOauth()],
+  output: "hybrid",
+  adapter: cloudflare()
 });
